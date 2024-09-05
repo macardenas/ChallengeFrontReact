07e508c517a79011a3b8ae447086487e8369bcb4
macardenas <michael.torrealba09@gmail.com>
2024-09-05 18:01:49 -0400

    ajuste

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    allowedHosts: 'all',
+    port: 4000
+  },
+
+  module: {
+    rules: [
+        {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/
+        },
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+        template: './src/index.html'
+    })
+  ]
+}
