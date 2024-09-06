a8b9b8eec5694276ea6d990ed7ecba5f5b9c95bc
Michel Cardenas <michael.torrealba09@gmail.com>
2024-09-06 13:20:58 -0400

    try catch

--- src/services/api.js
+++ src/services/api.js
@@ -1,27 +1,42 @@
 
 export const getList = async () => {
-    let resp = await fetch('http://localhost:3001/api/files/data', {
-        'Content-Type': 'application/json',
-        headers: { Authorization: 'Bearer estoesuntokenbearer' }
-      })
-      let data = await resp.json();
-      return data;
+    try {
+        let resp = await fetch('http://localhost:3001/api/files/data', {
+            'Content-Type': 'application/json',
+            headers: { Authorization: 'Bearer estoesuntokenbearer' }
+          })
+          let data = await resp.json();
+          return data;
+    } catch (error) {
+        console.error(error);
+    }
+    
 }
 
 export const FileList = async () => {
-  let resp = await fetch('http://localhost:3001/api/files/list', {
-      'Content-Type': 'application/json',
-      headers: { Authorization: 'Bearer estoesuntokenbearer' }
-    })
-    let data = await resp.json();
-    return data;
+    try {
+        let resp = await fetch('http://localhost:3001/api/files/list', {
+            'Content-Type': 'application/json',
+            headers: { Authorization: 'Bearer estoesuntokenbearer' }
+          })
+          let data = await resp.json();
+          return data;
+    } catch (error) {
+        console.error(error)
+    }
+  
 }
 
 export const SearchFile = async (value) => {
-  let resp = await fetch('http://localhost:3001/api/files/data/'+value, {
-      'Content-Type': 'application/json',
-      headers: { Authorization: 'Bearer estoesuntokenbearer' }
-    })
-    let data = await resp.json();
-    return data;
+    try {
+        let resp = await fetch('http://localhost:3001/api/files/data/'+value, {
+            'Content-Type': 'application/json',
+            headers: { Authorization: 'Bearer estoesuntokenbearer' }
+          })
+          let data = await resp.json();
+          return data;
+    } catch (error) {
+        console.error(error);
+    }
+ 
 }
